77d9e1f4811ffbf849b9314de87851db0c75a8b4
FICO\rafal <rafalficek082@gmail.com>
2023-05-23 23:46:52 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+const bg = document.getElementsByClassName("bg");
+const background = ["images/background.png", "images/background2.png"];
+const bggrid = ["images/bg-grid.png", "images/bg-grid2.png"];
+
+let currentIndex = 0;
+
+function changeBackgroundImage() {
+  currentIndex = (currentIndex + 1) % background.length;
+  header.style.backgroundImage = "url('" + background[currentIndex] + "')";
+  bg[0].style.backgroundImage = "url('" + bggrid[currentIndex] + "')";
+}
+
+setInterval(changeBackgroundImage, 3000); 
